89a3a5c147eaec13edd46cf5621ec645a5ec2062
Vladiradi <rzhvlad@gmail.com>
2025-06-26 00:53:02 +0200

    telran.project +GardenShopApplication.java

---
+++ src/main/java/telran/project/gardenshop/JavaTeamGardenshopApplication.java
@@ -0,0 +1,13 @@
+package telran.project.gardenshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeamGardenshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTeamGardenshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/project/gardenshop/JavaTeamGardenshopApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.project.gardenshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeamGardenshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a468b48b402783901df2c763358967e8a795873d
Vladiradi <rzhvlad@gmail.com>
2025-06-26 00:54:24 +0200

    +GardenShopApplication.java

--- src/main/java/telran/project/gardenshop/GardenShopApplication.java
+++ src/main/java/telran/project/gardenshop/GardenShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaTeamGardenshopApplication {
+public class GardenShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaTeamGardenshopApplication.class, args);
+		SpringApplication.run(GardenShopApplication.class, args);
 	}
 
 }

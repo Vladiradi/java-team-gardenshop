715f2668adcf9e1d5e7ad7de36a72eb40f5b45c7
Vladiradi <rzhvlad@gmail.com>
2025-06-27 00:59:13 +0200

    +CategoryController +CategoryService and new readme.md

---
+++ src/main/java/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package controller;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public CategoryResponceDto create(@Valid @RequestBody CategoryRequestDto dto) {
+        return categoryService.create(dto);
+    }
+
+    @GetMapping
+    public List<CategoryResponceDto> getAll() {
+        return categoryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponceDto getById(@PathVariable Long id) {
+        return categoryService.getById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+
+}
---
+++ src/main/java/service/CategoryService.java
@@ -0,0 +1,10 @@
+package service;
+
+import java.util.List;
+
+public interface CategoryService {
+    CategoryResponceDto create(CategoryRequestDto dto);
+    List<CategoryResponceDto> getAll();
+    CategoryResponceDto getById(Long id);
+    void delete(Long id);
+}

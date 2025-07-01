1dd3c97887ced04dbc398ab7c3c451fa051487af
Anna <boldtanna4@gmail.com>
2025-07-01 21:01:31 +0200

    task 1
    Adding dependencies and annotations

--- src/main/java/telran/project/gardenshop/dto/UserRequestDto.java
+++ src/main/java/telran/project/gardenshop/dto/UserRequestDto.java
@@ -1,9 +1,13 @@
 package telran.project.gardenshop.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import telran.project.gardenshop.enums.Role;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDto {
     private String email;
     private String password;
--- src/main/java/telran/project/gardenshop/dto/UserResponseDto.java
+++ src/main/java/telran/project/gardenshop/dto/UserResponseDto.java
@@ -1,9 +1,13 @@
 package telran.project.gardenshop.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import telran.project.gardenshop.enums.Role;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String email;

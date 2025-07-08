2b5f09d0532e736df8241c10b45478935977ff25
Vladiradi <rzhvlad@gmail.com>
2025-07-08 19:42:04 +0200

    resolve UserMapper problem

--- src/main/java/telran/project/gardenshop/dto/UserResponseDto.java
+++ src/main/java/telran/project/gardenshop/dto/UserResponseDto.java
@@ -1,16 +1,17 @@
 package telran.project.gardenshop.dto;
 
 import lombok.Data;
+import lombok.Builder;
 
 import java.util.List;
 
 @Data
+@Builder
 public class UserResponseDto {
     private Long id;
     private String fullName;
     private String email;
     private String phoneNumber;
     private String role;
-
     private List<FavoriteResponseDto> favorites;
 }

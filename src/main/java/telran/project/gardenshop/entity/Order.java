63ae6ba338c7b2f076021520b0c8cb86a58c2e1c
Vladiradi <rzhvlad@gmail.com>
2025-07-21 18:17:38 +0200

    GARDENSHOP-28: +OrderRepository +enums

---
+++ src/main/java/telran/project/gardenshop/entity/Order.java
@@ -0,0 +1,47 @@
+package telran.project.gardenshop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import telran.project.gardenshop.enums.OrderStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(exclude = "user")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private String deliveryMethod;
+
+    @Column(nullable = false)
+    private String deliveryAdress;
+
+    @Column(nullable = false)
+    private String contactName;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/telran/project/gardenshop/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package telran.project.gardenshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import telran.project.gardenshop.entity.Order;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
+
+    List<Order> findAllByStatusNotIn(Collection<OrderStatus> statuses);
+}

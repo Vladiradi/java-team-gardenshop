2386d78aeb5b169c45d8f19a1d8c4d20b9df1ee8
annaboldt <boldtanna4@gmail.com>
2025-07-22 12:05:09 +0200

    Merge pull request #27
    * Sprint 3 / task 1

---
+++ src/main/java/telran/project/gardenshop/dto/CartRequestDto.java
@@ -0,0 +1,8 @@
+package telran.project.gardenshop.dto;
+
+import lombok.Data;
+
+@Data
+public class CartRequestDto {
+    private Long userId;
+}
---
+++ src/main/java/telran/project/gardenshop/dto/CartResponseDto.java
@@ -0,0 +1,9 @@
+package telran.project.gardenshop.dto;
+
+import lombok.Data;
+
+@Data
+public class CartResponseDto {
+    private Long id;
+    private Long userId;
+}

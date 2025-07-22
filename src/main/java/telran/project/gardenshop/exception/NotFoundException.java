61e920c78ac07801e5e0762601e46d1840b11b60
Vladiradi <rzhvlad@gmail.com>
2025-07-22 18:05:08 +0200

    GARDENSHOP-28: add NotFoundException

--- src/main/java/telran/project/gardenshop/exception/NotFoundException.java
+++ src/main/java/telran/project/gardenshop/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package telran.project.gardenshop.exception;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import telran.project.gardenshop.entity.OrderItem;
+import telran.project.gardenshop.entity.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }

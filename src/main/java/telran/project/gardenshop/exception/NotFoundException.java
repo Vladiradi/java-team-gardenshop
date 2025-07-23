1481812fc948cc365a3a7841a7cca623cf4206e4
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-07-23 14:37:04 +0200

    TASK 4 – SPRINT 3: OrderItem delete unused exception

--- src/main/java/telran/project/gardenshop/exception/NotFoundException.java
+++
@@ -1,6 +0,0 @@
-package telran.project.gardenshop.exception;
-import org.springframework.data.jpa.repository.JpaRepository;
-import telran.project.gardenshop.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}

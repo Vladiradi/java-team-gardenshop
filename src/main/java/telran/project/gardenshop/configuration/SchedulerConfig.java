26eb9440d3baeedfbb5bd0cc272509197d395479
Vladiradi <rzhvlad@gmail.com>
2025-07-26 16:39:40 +0200

    add service for Scheduler and scheduler config

---
+++ src/main/java/telran/project/gardenshop/configuration/SchedulerConfig.java
@@ -0,0 +1,11 @@
+package telran.project.gardenshop.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
+
+
+
